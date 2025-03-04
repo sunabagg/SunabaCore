efd9fff5c5328a70132d773444127135be8e0aa4
Lucas C. Abbas <lucasbear9@gmail.com>
2025-03-04 01:43:31 -0900

    Add GameNode property to NetBehavior class

--- Sunaba/NetBehavior.cs
+++ Sunaba/NetBehavior.cs
@@ -2,6 +2,12 @@
 
 public class NetBehavior : IDisposable
 {
+    internal GameNode _gameNode;
+    
+    public GameNode GameNode { get => _gameNode; }
+    
+    public Scene Scene { get => _gameNode.Scene; }
+    
     public virtual void Initialize() { }
     
     public virtual void EnterScene() { }
